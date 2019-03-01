bce32c594f2d12e8eb78b724e9f6d9d1399978f8
Victor Cerevicinii <cr@retent.ly>
2019-03-01 17:17:20 +0200

    Updates

---
+++ precache-manifest.d49127980958913b3f51fcc7ff471afb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a3771ac45f5c7f04f6a",
+    "url": "/gh-react/static/js/runtime~main.2a3771ac.js"
+  },
+  {
+    "revision": "182172842da018acec13",
+    "url": "/gh-react/static/js/main.18217284.chunk.js"
+  },
+  {
+    "revision": "ea26bfdea7aae1ac9183",
+    "url": "/gh-react/static/js/2.ea26bfde.chunk.js"
+  },
+  {
+    "revision": "182172842da018acec13",
+    "url": "/gh-react/static/css/main.917cdd17.chunk.css"
+  },
+  {
+    "revision": "05d3837b3ffc9625c110720db5ffc98a",
+    "url": "/gh-react/index.html"
+  }
+];
