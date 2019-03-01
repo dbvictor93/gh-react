c69ca3bd63745633bc4692441ead7b701209d84b
Victor Cerevicinii <cr@retent.ly>
2019-03-01 17:15:46 +0200

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import loremIpsum from 'lorem-ipsum';
+import { List } from 'react-virtualized';
+
+const rowCount = 1000;
 
 class App extends Component {
+    constructor() {
+        super();
+        this.list = Array(rowCount).fill().map((val, idx) => {
+            return {
+                id: idx,
+                name: 'John Doe',
+                image: `https://via.placeholder.com/60/5398f0/FFFFFF/?text=${idx}`,
+                text: loremIpsum({
+                    count: 1,
+                    units: 'sentences',
+                    sentenceLowerBound: 4,
+                    sentenceUpperBound: 8
+                })
+            }
+        });
+
+        this.renderRow = this.renderRow.bind(this);
+    }
+
+    renderRow({key,style,index}) {
+        return (
+            <div key={key} style={style} className="row">
+                <div className="image">
+                    <img src={this.list[index].image} alt="" />
+                </div>
+                <div className="content">
+                    <div>{this.list[index].name}</div>
+                    <div>{this.list[index].text}</div>
+                </div>
+            </div>
+        );
+    }
+
   render() {
+      const listHeight = 600;
+      const rowHeight = 50;
+      const rowWidth = 800;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <div className="list">
+              {this.list.length > 0 &&
+                  <List
+                      width={rowWidth}
+                      height={listHeight}
+                      rowHeight={rowHeight}
+                      rowRenderer={this.renderRow}
+                      rowCount={this.list.length}
+                  />
+              }
+
+          </div>
+        </div>
     );
   }
 }
